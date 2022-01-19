51eeb6e20466c3f8e0191e2be9e7e55e9c27bfd2
JackieLWJ <3069204479@qq.com>
2022-01-19 22:49:15 +0800

    c语言概述-编程练习

---
+++ test_1_19/test_1_19/test.c
@@ -0,0 +1,114 @@
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	/*int num;
+//	num = 1;
+//	printf("I am a simple");
+//	printf("computer.\n");
+//	printf("My favorite number is %d because it is first,\n", num);*/
+//	 
+//	/*第一题
+//	printf("李文杰\n");
+//	printf("李\n文杰\n");
+//	printf("李");
+//	printf("文杰\n");
+//	*/
+//	/*第二题
+//	printf("李文杰\n中国\n");
+//	*/
+//	/*第三题
+//	int year = 20;
+//	printf("年龄:%d\n",year);
+//	printf("天数:%d\n", year * 365);
+//	*/
+//	
+//	return 0;
+//}
+//第四题
+// #include <stdio.h>
+//void jolly()
+//{
+//	printf("For he's a jolly good fellow!\n");
+//
+//}
+//void deny()
+//{
+//	printf("Which nobody can deny!\n");
+//}
+//int main()
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	
+//	return 0;
+//
+//}
+// 第五题
+//#include<stdio.h>
+//void br()
+//{
+//	printf("Brzail,Russia");
+//}
+//void ic()
+//{
+//	printf("India,China");
+//}
+//int main ()
+//{
+//	br();
+//	printf(",");
+//	ic();
+//	printf("\n");
+//	ic();
+//	printf("\n");
+//	br();
+//	return 0;
+//}
+// 第六题
+//#include <stdio.h>
+//int main()
+//{
+//	int toes=10;
+//	printf("%d\n", toes);
+//	printf("%d\n", toes*toes);
+//	printf("%d\n", toes*toes*toes);
+//	return 0;
+//}
+// 第七题
+//#include <stdio.h>
+//int main ()
+//{
+//	printf("Smile!");
+//	printf("Smile!");
+//	printf("Smile!");
+//	printf("\n");
+//	printf("Smile!");
+//	printf("Smile!");
+//	printf("\n");
+//	printf("Smile!");
+//	return 0;
+//}
+// 第八题
+//#include <stdio.h>
+//void two();
+//void one_three()
+//{
+//	printf("one\n");
+//	two();
+//	printf("three\n");
+//}
+//void two()
+//{
+//	printf("two\n");
+//
+//}
+//int main()
+//{
+//	printf("strarting now:\n");
+//	one_three();
+//	printf("done!\n");
+//	return 0;
+//}
