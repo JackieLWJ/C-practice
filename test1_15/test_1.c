f057a0a5bee3902dc3ba921d30ce58b510a78519
JackieLWJ <3069204479@qq.com>
2022-01-19 19:41:01 +0800

    依旧是测试

---
+++ test1_15/test_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	/*double d = 3.14;
+	double* pd = &d;*/
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(short*));
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(double*));
+	return 0;
+}
