066737a8eee7fabd319ec5a57b07e272b3ee861f
JackieLWJ <3069204479@qq.com>
2022-01-20 21:21:15 +0800

    分支和循环

--- test_1_20/test_1.c
+++ test_1_20/test_1.c
@@ -147,29 +147,30 @@
 //	printf("%s\n", arr2);
 //	return 0;
 //}
-//#include <stdio.h>
-//int main()
-//{
-//	char password[20] = { 0 };
-//	int i = 0;
-//	for (i = 0; i < 3; i++)
-//	{
-//		printf("ÇëÊäÈëÃÜÂë\n");
-//		scanf("%s", password);
-//		if (strcmp(password,"123456")==0)
-//		{
-//			printf("µÇÂ½³É¹¦\n");
-//			break;
-//		}
-//		else
-//		{
-//			printf("ÃÜÂë´íÎó\n");
-//		}
-//		if(i==3)
-//		{
-//			printf("Èý´ÎÃÜÂë¾ù´íÎó,ÇëÖØÊÔ\n");
-//		}
-//		
-//	}
-//	return 0;
-//}
+// 
+#include <stdio.h>
+int main()
+{
+	char password[20] = { 0 };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("ÇëÊäÈëÃÜÂë\n");
+		scanf("%s", password);
+		if (strcmp(password,"123456")==0)
+		{
+			printf("µÇÂ½³É¹¦\n");
+			break;
+		}
+		else
+		{
+			printf("ÃÜÂë´íÎó\n");
+		}
+		if(i==3)
+		{
+			printf("Èý´ÎÃÜÂë¾ù´íÎó,ÇëÖØÊÔ\n");
+		}
+		
+	}
+	return 0;
+}
